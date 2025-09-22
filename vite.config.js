0ef96fba386973916420bcfe25156013c86c6d3f
Liam Tietjens <l.tietjens@hotmail.com>
2025-09-22 13:44:30 +1200

    corrected doamins to explisitly allow domains

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     preview: {
-        allowedHosts: ["nzdev.org", "*.nzdev.org", "127.0.0.1", "localhost"]
+        allowedHosts: ["fish.nzdev.org", "fishy.nzdev.org", "127.0.0.1", "localhost"]
     }
 })
